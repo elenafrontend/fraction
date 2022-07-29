67bd0767480f4dacb85593d0e5edc8c096007d39
gumerova.e <gumerova.e@etton.ru>
2022-07-29 13:50:42 +0300

    feat(header): implement svg logo

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,25 @@
 <template lang="pug">
   .wrapper
+    SvgSprite
     PageHeader
     main.page-content
       .container
         section.page-section
           .page-banner
-            img(src="@/assets/images/4-3.jpg")
+            img(src="@/assets/images/banner/hero-image.jpg")
         section.page-section
           FractionList
 </template>
 
 <script>
+import SvgSprite from '@/components/SvgSprite.vue';
 import PageHeader from '@/components/PageHeader.vue';
 import FractionList from '@/components/FractionList.vue';
 
 export default {
   name: 'App',
   components: {
+    SvgSprite,
     PageHeader,
     FractionList,
   },
